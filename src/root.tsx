8f5966857910d7700d4621cf1741f36ba9500906
sky9-labs <aakashmukund@gmail.com>
2023-03-06 19:03:04 +0530

    self messages

--- src/root.tsx
+++ src/root.tsx
@@ -24,7 +24,7 @@ export default function Root() {
   return (
     <Html lang="en">
       <Head>
-        <Title>SolidStart - With TailwindCSS</Title>
+        <Title>younme </Title>
         <Meta charset="utf-8" />
         <Meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
@@ -36,9 +36,9 @@ export default function Root() {
                 <li class={`border-b-2 ${active("/")} mx-1.5 sm:mx-6`}>
                   <A href="/">Home</A>
                 </li>
-                <li class={`border-b-2 ${active("/about")} mx-1.5 sm:mx-6`}>
+                {/* <li class={`border-b-2 ${active("/about")} mx-1.5 sm:mx-6`}>
                   <A href="/about">About</A>
-                </li>
+                </li> */}
               </ul>
             </nav>
             <Routes>
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,31 +1,73 @@
 import { A } from "solid-start";
+import { createSignal,For } from "solid-js";
+
 import Counter from "~/components/Counter";
 
+type MessageType = { text: string; me: boolean};
+
 export default function Home() {
+  const [messages, setMessages] = createSignal([] as MessageType[]);
+
+  const HandelUserMessageInput = (key: any) =>{
+    const message = (document.querySelector("#userMessageInput") as HTMLInputElement).value
+    if(key?.code === "Enter" && message)HandelUserMessage(message) 
+  }
+
+  const HandelUserMessage = (message: string) =>{
+    const userVariable = parseInt(Math.random() * 10) < 5 ? false : true
+    let newMessage :MessageType = {text:message, me: userVariable}
+    setMessages([...messages(),newMessage]); 
+    (document.querySelector("#userMessageInput") as HTMLInputElement).value = ""
+    const scroolElemen = (document.querySelector("#MessageArea") as HTMLInputElement)
+    scrollTo(0, scroolElemen.scrollHeight);
+  }
+
   return (
     <main class="text-center mx-auto text-gray-700 p-4">
       <h1 class="max-6-xs text-6xl text-sky-700 font-thin uppercase my-16">
-        Hello world!
+        Hello Georgia!
       </h1>
-      <Counter />
-      <p class="mt-8">
-        Visit{" "}
-        <a
-          href="https://solidjs.com"
-          target="_blank"
-          class="text-sky-600 hover:underline"
-        >
-          solidjs.com
-        </a>{" "}
-        to learn how to build Solid apps.
-      </p>
-      <p class="my-4">
-        <span>Home</span>
-        {" - "}
-        <A href="/about" class="text-sky-600 hover:underline">
-          About Page
-        </A>{" "}
-      </p>
+      {/* <Counter /> */}
+      <div class="mt-8 overflow-auto" id="MessageArea">
+        <ul>
+          <For each={messages()}>
+            {(message,index)=>(
+              <li class="mt-8">
+                <p 
+                  class={message.me ? "text-right" : "text-left"}
+                  style={{
+                    "background": message.me ? "skyblue" : "#F9629F",
+                    "border": `5px solid ${message.me ? "skyblue" : "#F9629F"}`,
+                    "border-radius": message.me ? "10px 20px 0px 30px": "20px 10px 30px 0px",
+                    "padding": "0px 12px",
+                  }}
+                >{message.text}</p>
+              </li>
+            )}
+          </For>
+        </ul>
+      </div>
+      <br />
+      <br />
+      <div class="my-4"
+      style={{
+        position: "fixed",
+        bottom:0,
+        width:"95%"
+      }}>
+      <input 
+            id="userMessageInput"
+            onKeyDown={HandelUserMessageInput}
+            placeholder="What's on your mind"
+            style={{
+              width: "100%",
+              height: "30px",
+              padding: "0px 15px",
+              border:"1px solid black",
+              "border-radius": "15px",
+            }}
+          />   
+      </div>
     </main>
   );
 }
