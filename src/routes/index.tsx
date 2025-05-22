c6c82db9ec7fa39ea3ad079d8485c4ed590efe22
Mukund <Mukund.sdx@gmail.com>
2025-05-22 15:45:01 +0530

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <main class="dark text-center mx-auto text-gray-700 p-4 bg-midnight min-h-max relative">
       <h1 class="max-6-xs text-6xl text-sky-700 font-thin uppercase my-16">
-        You Truly are my beginning my middle and end!
+        " You Truly are my beginning my middle and end! "
       </h1>
       {/* <Counter /> */}
         <img src="https://tailwindcss.com/_next/static/media/docs@tinypng.d9e4dcdc.png" alt="" class="w-[71.75rem] flex-none max-w-none dark:hidden" decoding="async"></img>
